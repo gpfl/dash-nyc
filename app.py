cd0fc4f692ff88a7f8400b6b3f795d9548c7cfbc
Gustavo Flores <gustavopereiraflores@gmail.com>
2019-10-17 21:41:25 -0300

    "New grid, new chart

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 import pandas as pd
 
 # Data imports
-nyc_data = pd.read_csv('data/AB_NYC_2019.csv')
+#nyc_data = pd.read_csv('data/AB_NYC_2019.csv')
 
 # Flask app
 app = Flask(__name__, template_folder="")
 
 @app.route('/')
 def index():
-    return render_template("index.html", data=nyc_data)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
